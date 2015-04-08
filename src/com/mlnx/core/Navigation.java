7f3e60e275a9ff70f32b2ff175f9bcfc69e36f22
shule0000 <460968755@qq.com>
2015-04-08 09:20:32 +0800

    导航栏稍作修改。

--- src/com/mlnx/core/Navigation.java
+++ src/com/mlnx/core/Navigation.java
@@ -7,90 +7,82 @@ import static javax.persistence.GenerationType.IDENTITY;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 /**
  * Navigation entity. @author MyEclipse Persistence Tools
  */
 @Entity
-@Table(name="navigation"
-    ,catalog="mlnx"
-)
+@Table(name = "navigation", catalog = "mlnx")
+public class Navigation implements java.io.Serializable {
 
-public class Navigation  implements java.io.Serializable {
+	// Fields
 
+	private Integer nid;
+	private String navName;
+	private Integer navPriority;
+	private Integer navType;
+	private Integer navStatus;
 
-    // Fields    
+	// Constructors
 
-     private Integer nid;
-     private String navName;
-     private Integer navPriority;
-     private Integer navStatus;
+	/** default constructor */
+	public Navigation() {
+	}
 
+	/** full constructor */
+	public Navigation(String navName, Integer navPriority, Integer navType,
+			Integer navStatus) {
+		this.navName = navName;
+		this.navPriority = navPriority;
+		this.navType = navType;
+		this.navStatus = navStatus;
+	}
 
-    // Constructors
+	// Property accessors
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "nId", unique = true, nullable = false)
+	public Integer getNid() {
+		return this.nid;
+	}
 
-    /** default constructor */
-    public Navigation() {
-    }
+	public void setNid(Integer nid) {
+		this.nid = nid;
+	}
 
-    
-    /** full constructor */
-    public Navigation(String navName, Integer navPriority, Integer navStatus) {
-        this.navName = navName;
-        this.navPriority = navPriority;
-        this.navStatus = navStatus;
-    }
+	@Column(name = "navName", nullable = false, length = 50)
+	public String getNavName() {
+		return this.navName;
+	}
 
-   
-    // Property accessors
-    @Id @GeneratedValue(strategy=IDENTITY)
-    
-    @Column(name="nId", unique=true, nullable=false)
+	public void setNavName(String navName) {
+		this.navName = navName;
+	}
 
-    public Integer getNid() {
-        return this.nid;
-    }
-    
-    public void setNid(Integer nid) {
-        this.nid = nid;
-    }
-    
-    @Column(name="navName", length=50)
+	@Column(name = "navPriority", nullable = false)
+	public Integer getNavPriority() {
+		return this.navPriority;
+	}
 
-    public String getNavName() {
-        return this.navName;
-    }
-    
-    public void setNavName(String navName) {
-        this.navName = navName;
-    }
-    
-    @Column(name="navPriority")
+	public void setNavPriority(Integer navPriority) {
+		this.navPriority = navPriority;
+	}
 
-    public Integer getNavPriority() {
-        return this.navPriority;
-    }
-    
-    public void setNavPriority(Integer navPriority) {
-        this.navPriority = navPriority;
-    }
-    
-    @Column(name="navStatus")
+	@Column(name = "navType", nullable = false)
+	public Integer getNavType() {
+		return this.navType;
+	}
 
-    public Integer getNavStatus() {
-        return this.navStatus;
-    }
-    
-    public void setNavStatus(Integer navStatus) {
-        this.navStatus = navStatus;
-    }
-   
+	public void setNavType(Integer navType) {
+		this.navType = navType;
+	}
 
+	@Column(name = "navStatus", nullable = false)
+	public Integer getNavStatus() {
+		return this.navStatus;
+	}
 
-
-
-
-
-
+	public void setNavStatus(Integer navStatus) {
+		this.navStatus = navStatus;
+	}
 
 }
